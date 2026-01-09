a0deece246794bc55d1655f3b4efec02881d6f2e
ANANTA FIRDAUS <ananta23ti@mahasiswa.pcr.ac.id>
2026-01-10 04:05:23 +0700

    update navbar 2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-export default {
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
   build: {
     sourcemap: true,
-  }
-}
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        map: resolve(__dirname, "map.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        news: resolve(__dirname, "news.html"),
+      },
+    },
+  },
+});
